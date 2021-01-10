b481b95063806d39864de6c749463c301a6f5310
hank9999 <hank9999@qq.com>
2021-01-10 23:17:21 +0800

    Init

---
+++ src/main/java/com/github/hank9999/botconnector/BotConnector.java
@@ -0,0 +1,17 @@
+package com.github.hank9999.botconnector;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BotConnector extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
